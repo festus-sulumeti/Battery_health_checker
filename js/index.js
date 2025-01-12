cbb5187bcc3b3e31df8acaf08c0f00c006d4b743
festus-sulumeti <sulumetifestus@outlook.com>
2025-01-12 11:52:49 +0300

    Fix: typo in comment for updating battery level percentage

--- js/index.js
+++ js/index.js
@@ -8,29 +8,41 @@ function initBattery(){
     
     navigator.getBattery().then((batt) =>{
         updateBattery = () =>{
-            /* 1. Update the battery level percentage displayed */
+            /* 
+             * 1. Update the battery level percentage displayed 
+             */
             let level = Math.floor(batt.level * 100)
             batteryPercentage.innerHTML = level + '%'
 
-            /* 2. Update the battery liquid height to reflect the current level */
+            /* 
+             * 2. Update the battery liquid height to reflect the current level 
+             */
             batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`
 
-            /* 3. Display battery status messages based on the battery level and charging state */
-            if(level == 100){ /* Battery is fully charged */
+            /* 
+             * 3. Display battery status messages based on the battery level and charging state 
+             */
+            if(level == 100){ 
+                /* Battery is fully charged */
                 batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
                 batteryLiquid.style.height = '103%' /* Slightly overflow to hide the ellipse */
             }
-            else if(level <= 20 && !batt.charging){ /* Battery is low and not charging */
+            else if(level <= 20 && !batt.charging){ 
+                /* Battery is low and not charging */
                 batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
             }
-            else if(batt.charging){ /* Battery is currently charging */
+            else if(batt.charging){ 
+                /* Battery is currently charging */
                 batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`
             }
-            else{ /* Battery is neither low nor charging */
+            else{ 
+                /* Battery is neither low nor charging */
                 batteryStatus.innerHTML = ''
             }
             
-            /* 4. Change the battery liquid color based on the battery level */
+            /* 
+             * 4. Change the battery liquid color based on the battery level 
+             */
             if(level <= 20){
                 batteryLiquid.classList.add('gradient-color-red')
                 batteryLiquid.classList.remove('gradient-color-orange','gradient-color-yellow','gradient-color-green')
@@ -50,7 +62,9 @@ function initBattery(){
         }
         updateBattery()
 
-        /* 5. Add event listeners to update the battery status when it changes */
+        /* 
+         * 5. Add event listeners to update the battery status when it changes 
+         */
         batt.addEventListener('chargingchange', () => {updateBattery()})
         batt.addEventListener('levelchange', () => {updateBattery()})
     })
