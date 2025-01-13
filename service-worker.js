0a69d4e1fc6daf65974e46a15b7c64c6566fdf38
festus-sulumeti <sulumetifestus@outlook.com>
2025-01-13 11:11:42 +0300

    feat: Create a service-worker.js file to handle caching.

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'battery-health-checker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/index.js',
+  '/js/battery-info.js',
+  '/js/notification.js',
+  '/image/bat.jpeg',
+  // Add other assets to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
